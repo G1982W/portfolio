793784a0f9983f0ce262c679f4c246414595937f
c0mac <phoenix.never9@gmail.com>
2025-09-14 13:01:02 -0700

    Add askblake.mp4 video asset

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -199,7 +199,7 @@ export const DATA = {
       image: "",
       images: [],
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+        "https://cdn.jsdelivr.net/gh/c0mac/greg-assets@v1.0.4/Logos/askblake.mp4",
     },
     {
       title: "Intermountain Health",
